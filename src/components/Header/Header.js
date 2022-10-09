5afe9d540dd6919c2fb056398346204436c36704
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-09 16:12:40 +0600

    responsive navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import NavBar from "../NavBar/NavBar";
+import { Bars3Icon, XCircleIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      <div>
-        <h1>Logo</h1>
+    <div className=" py-5 bg-lime-600">
+      <div className="container w-5/6 flex justify-between items-center mx-auto">
+        <div className="w-1/5">
+          <h1 className="text-4xl font-bold text-white">Food</h1>
+        </div>
+        <NavBar navOptions={open}></NavBar>
+        <Bars3Icon
+          className={`h-10 w-10 md:hidden text-white ${
+            open ? "hidden" : "static"
+          }`}
+          onClick={() => setOpen(!open)}
+        />
+        <XCircleIcon
+          className={`h-10 w-10 md:hidden text-white ${
+            open ? "static" : "hidden"
+          }`}
+          onClick={() => setOpen(!open)}
+        />
       </div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/orders">Orders</Link>
-      </nav>
     </div>
   );
 };
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = ({ navOptions }) => {
+  return (
+    <ul
+      className={`bg-lime-500 z-10 md:bg-lime-600 w-3/5 md:w-1/5 md:flex absolute  md:static ease-in duration-300 ${
+        navOptions ? "left-0 top-0 h-full" : "left-[-240px] top-0 h-full"
+      }`}
+    >
+      <li className="md:mx-1 text-xl md:my-0 my-20 px-7 md:px-2">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="md:mx-1 text-xl md:my-0 my-20 px-7 md:px-2">
+        <Link to="/orders">Orders</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
