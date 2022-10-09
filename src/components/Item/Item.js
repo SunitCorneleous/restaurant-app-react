df5271bed99e263c5220e1d0100eff1c3dab29ed
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-09 23:08:57 +0600

    font size updated

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -13,8 +13,8 @@ const Item = ({ meal, price }) => {
         <img className="w-11/12 rounded-md" src={strMealThumb} alt="food" />
       </div>
       <div className="basis-3/5 h-full flex flex-col justify-between">
-        <h1 className="text-2xl">{name[0] + " " + name[1]}</h1>
-        <h4 className="text-xl">$ {price}</h4>
+        <h1 className="text-xl">{name[0] + " " + name[1]}</h1>
+        <h4 className="text-lg">$ {price}</h4>
         <button
           onClick={() => addToCartHandler(meal)}
           className="py-1 bg-lime-600 hover:bg-orange-500 rounded-md text-white text-sm"
