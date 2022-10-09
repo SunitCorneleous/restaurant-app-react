8bfa0418692ad19f5be7a835f66b1e17d286ab8e
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-09 23:30:26 +0600

    total meals in cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,9 +5,15 @@ import CartItem from "../CartItem/CartItem";
 const Cart = () => {
   const { cart } = useContext(CartContext);
 
+  let totalMeals = 0;
+
+  for (const item of cart) {
+    totalMeals += item.quantity;
+  }
+
   return (
     <div className="my-3">
-      <h2>Total selected meals : {cart.length}</h2>
+      <h2>Total selected meals : {totalMeals}</h2>
       {/* cart items */}
       <div className="my-5 md:block hidden">
         {cart.map(item => (
