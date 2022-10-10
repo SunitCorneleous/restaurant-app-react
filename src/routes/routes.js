61eb6eb805c21193e9e5e2fb3b6df03edcc04b35
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-10 14:49:12 +0600

    error component added with route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
-import { createBrowserRouter, Link } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Error from "../components/Error/Error";
 import Home from "../components/Home/Home";
 import ReviewOrder from "../components/ReviewOrder/ReviewOrder";
 import Main from "../layouts/Main";
@@ -17,16 +18,7 @@ export const router = createBrowserRouter([
       { path: "/orders", element: <ReviewOrder></ReviewOrder> },
       {
         path: "/*",
-        element: (
-          <div>
-            <h1 className="text-5xl my-14 font-semibold text-red-600">
-              Page Not Found
-            </h1>
-            <Link to="/" className="hover:underline text-2xl">
-              Back to Home
-            </Link>
-          </div>
-        ),
+        element: <Error></Error>,
       },
     ],
   },
